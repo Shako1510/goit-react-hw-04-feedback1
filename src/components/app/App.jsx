923395cd2ae8ef4462f8a7cf28b608a9b16b6220
Shako Yevhen <schako15101992@gmail.com>
2022-12-13 17:11:49 +0200

    finish feadback/

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,64 +1,89 @@
 import { ContainerApp } from './AppStyled';
-import React, { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "../feedbackOptions/FeedbackOptions";
 import Section from "../sectionFeedback/Section";
 import Statistics from "../statistics/Statistics";
 import Notification from '../notification/Notification';
 
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+export function App() {
+
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = (feedbackName) => {
+    switch (feedbackName) {
+      case "good":
+        return setGood((prev) => prev + 1);
+      case "neutral":
+        return setNeutral((prev) => prev + 1);
+      case "bad":
+        return setBad((prev) => prev + 1);
+      default:
+        return;
+    }
   };
 
 
-
-  handleIncrement = (e) => {
-    this.setState(prevState => {
-      return {
-        [e]: prevState[e] + 1,
-      };
-    });
-  };
-
-
-  calculateTotal = () => Object.values(this.state).reduce((acc, value) => acc + value, 0);
-
-
-  countPercentage() {
-    const { good } = this.state;
-    const total = this.calculateTotal();
+  const countPercentage = () => {
+    const total = calculateTotal();
     return Math.round((good * 100) / total);
+
   }
 
-  render() {
-    const total = this.calculateTotal();
-    const percentage = this.countPercentage();
+  const calculateTotal = () => {
+    return good + neutral + bad
+  };
 
-    return (
-      <ContainerApp>
-        <Section title="Please leave feedback">
+  const options = ['good', 'neutral', 'bad'];
 
-          <FeedbackOptions
-            onBtnClick={this.handleIncrement}
-            options={[...Object.keys(this.state)]}
+  return (
+
+    <ContainerApp>
+      <Section title="Please leave feedback">
+
+        <FeedbackOptions
+          onBtnClick={handleIncrement}
+          // onBtnClick={() => handleIncrement()}
+          options={options}
+        />
+        {calculateTotal() === 0 ? <Notification message="There is no feedback" /> :
+          <Statistics
+            goodValue={good}
+            neutralValue={neutral}
+            badValue={bad}
+            total={calculateTotal()}
+            percentage={countPercentage()}
           />
-
-
-          {this.calculateTotal() === 0 ? <Notification message="There is no feedback" /> :
-            <Statistics
-              goodValue={this.state.good}
-              neutralValue={this.state.neutral}
-              badValue={this.state.bad}
-              total={total}
-              percentage={percentage}
-            />
-          }
-
-        </Section>
-      </ContainerApp>
-    )
-  }
+        }
+      </Section>
+    </ContainerApp >
+  )
 };
+
+export default App;
+
+  // export class App extends Component {
+  //   state = {
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0
+  //   };
+
+  //   handleIncrement = (e) => {
+  //     this.setState(prevState => {
+  //       return {
+  //         [e]: prevState[e] + 1,
+  //       };
+  //     });
+  //   };
+
+  //   calculateTotal = () => Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+  //   countPercentage() {
+  //     const { good } = this.state;
+  //     const total = this.calculateTotal();
+  //     return Math.round((good * 100) / total);
+  //   }
